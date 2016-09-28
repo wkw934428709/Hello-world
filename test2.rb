1eb0db6d1a693550825d6d9555574355eb244b5d
Fang Yunong <yunong@codeaurora.org>
2016-09-28 19:50:44 +0800

    Test push into remote branch dev

--- test2.rb
+++ test2.rb
@@ -1,1 +1,5 @@
 Show the diferent merge not the fast--forward
+
+test branch in remote
+just create a branch in hello_world and this change will push into it.
+This also test git status came up 'by a commit ' as u put a commit but didnt push.
